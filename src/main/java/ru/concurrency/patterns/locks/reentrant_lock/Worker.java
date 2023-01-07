fea01ee9ac6d2617b987bc1e49e03a49c3a6046d
GordienkoEE <gordienko.e.evg@sberbank.ru>
2023-01-07 17:36:48 +0300

    ReentrantLock example fix

--- src/main/java/ru/concurrency/patterns/locks/reentrant_lock/Worker.java
+++ src/main/java/ru/concurrency/patterns/locks/reentrant_lock/Worker.java
@@ -2,26 +2,26 @@ package ru.concurrency.patterns.locks.reentrant_lock;
 
 import java.util.concurrent.locks.ReentrantLock;
 
-class Worker implements Runnable{
+class Worker implements Runnable {
 
     ReentrantLock lock;
 
     public Worker(ReentrantLock lock) {
-        this.lock=lock;
+        this.lock = lock;
     }
 
-    public void run(){
+    public void run() {
 
-        System.out.println(Thread.currentThread().getName() + " is waiting to get the lock");
+        System.out.println(Thread.currentThread().getName() + " is waiting to get the lock...");
         lock.lock();
-        System.out.println(Thread.currentThread().getName() + " has got the  lock.");
+        System.out.println(Thread.currentThread().getName() + " has got the  lock");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println(Thread.currentThread().getName() + " has queued Threads = " + lock.hasQueuedThreads());
-        System.out.println(Thread.currentThread().getName() + " has released the lock.");
+        System.out.println(Thread.currentThread().getName() + " has released the lock");
         lock.unlock();
     }
 }
